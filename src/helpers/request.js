12d49d61e13efa2667e16206aaf2cc4e2c85a037
zooeydotmango <newzoe@qq.com>
2019-10-24 02:39:17 +0800

    修改申请链接为https，修改config的默认地址

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {Message} from 'element-ui'
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-axios.defaults.baseURL = 'http://blog-server.hunger-valley.com';
+axios.defaults.baseURL = 'https://blog-server.hunger-valley.com';
 axios.defaults.withCredentials = true;
 
 export default function request(url, type = 'GET', data = {}) {
